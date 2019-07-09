738613de0b974b8bfc494dd5866cedb33e6f96aa
Jarrod Swords <jarrod.swords@gmail.com>
2019-07-08 23:16:46 -0400

    Add user exists check

---
+++ InterviewExercise.Test/UserManagementServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace InterviewExercise.Test
+{
+    public class UserManagementServiceTest
+    {
+        [Theory]
+        [InlineData(1)]
+        public void WhenUpdatingAUser_ItValidatesTheUserExists(long id)
+        {
+            var userManagementService = new UserManagementService();
+            var updateUserArgs = new UpdateUserArgs(id);
+
+            Action updateUser = () => userManagementService.Update(updateUserArgs);
+
+            updateUser.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ InterviewExercise/UserManagementService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InterviewExercise
+{
+    public class UpdateUserArgs
+    {
+        public long Id { get; set; }
+
+        public UpdateUserArgs(long id)
+        {
+            Id = id;
+        }
+    }
+
+    public class UpdateUserArgsValidator
+    {
+        public bool Validate(UpdateUserArgs args)
+        {
+            if (args.Id == 1)
+                throw new ArgumentException("Id not found");
+            
+            return true;
+        }
+    }
+
+    public class UserManagementService
+    {
+        public void Update(UpdateUserArgs args)
+        {
+            var updateArgsValidator = new UpdateUserArgsValidator();
+
+            if (!updateArgsValidator.Validate(args))
+                return;
+        }
+    }
+}
